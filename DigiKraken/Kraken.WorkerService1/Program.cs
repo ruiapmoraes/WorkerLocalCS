61d05a91f04af9e2de0a168128be7fb2c14d1c6f
Rui Moraes <rui-moraes@live.com>
2023-05-11 22:26:13 -0300

    Initial
    Adicionando o primeiro worker

---
+++ DigiKraken/Kraken.WorkerService1/Program.cs
@@ -0,0 +1,10 @@
+using Kraken.WorkerService1;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
